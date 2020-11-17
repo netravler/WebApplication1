8c98884eee05ceb3137a7ed0ae3e8ebf807711c7
netravler <pheintz@hotmail.com>
2020-11-17 09:07:09 -0600

    Add files via upload

---
+++ Models/FireFighter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class FireFighter
+    {
+            public long IdEmail { get; set; }
+            public string Email { get; set; }
+            public bool IsCompleteEmail { get; set; }
+    }
+}
